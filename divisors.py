6e01dcd55354fe72448ef91d751410b58efca671
Austin McCalley <austin@austinmccalley.com>
2021-04-15 17:04:47 -0700

    Finish divisors.py, imported math lib for flooring

--- divisors.py
+++ divisors.py
@@ -1,6 +1,16 @@
+import math
+
+
 def calcDivisors(n):
     divisiors = []
-    for i in range(1, n / 2):
+    for i in range(1, math.floor(n / 2)):
         if n % i == 0:
             divisiors.append(i)
     return divisiors
+
+
+d = calcDivisors(20)
+
+
+for i in range(len(d)):
+    print(d[i])
